07980cab2c69e76602a9a5a9e3c097dfb3764830
Shaharyar-saleem <shaharyarsaleem71@gmail.com>
2021-06-30 02:44:44 +0500

    fix(cards, responsivness): dashboard cards, responsivness fixed

--- dashboard/assets/js/script.js
+++ dashboard/assets/js/script.js
@@ -13,7 +13,7 @@ $(window).scroll(function(){
 
  // Disclaimer modal
  var modal = document.getElementById("myModal");
- var btn = document.getElementById("myBtn");
+ var btn = document.getElementById("connectBtn");
  var span = document.getElementsByClassName("close")[0];
  btn.onclick = function() {
    modal.style.display = "block";
