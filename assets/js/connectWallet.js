9ed64b35c294d7aaf94c8aeac276c315efee3e1b
Shaharyar-saleem <shaharyarsaleem71@gmail.com>
2021-08-01 19:05:28 +0500

    fix(warning): warning msg logic updated

--- assets/js/connectWallet.js
+++ assets/js/connectWallet.js
@@ -115,7 +115,7 @@ async function fetchAccountData() {
     document.querySelector("#connected").style.display = "block";
 
     // Display Network Error
-    if (chainId != 56) {
+    if (chainId != 56 || chainId != 97) {
         document.querySelector("#prepare").style.display = "none";
         document.querySelector("#connected").style.display = "none";
         document.querySelector("#networkError").style.display = "block";
