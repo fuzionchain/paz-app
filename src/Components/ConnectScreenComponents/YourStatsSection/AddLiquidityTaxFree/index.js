3136efd2b05e65caba66fa3bd0ee303c633749fb
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2021-10-04 23:52:17 +0500

    feat: Liquidity Tax Free section added

---
+++ src/Components/ConnectScreenComponents/YourStatsSection/AddLiquidityTaxFree/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div class="col-md-6 mt-3">
+      <div class="stat-card">
+        <div class="media-body pt-3">
+          <p class="stat-card-subheading mb-3">
+            Add Liquidity Tax Free Section
+          </p>
+          <div className="your-stat-card-1" style={{ height: "275px" }}>
+            <div className="media">
+              <div className="media-body pt-3">
+                <p className="your-stat-subheading mb-0">Total PAZ Holdings</p>
+              </div>
+            </div>
+          </div>
+          <p class="stat-card-subheading mb-3 mt-3">
+            Add Liquidity Tax Free Section
+          </p>
+          <p class="stat-card-subheading mb-3">
+            Add Liquidity Tax Free Section
+          </p>
+          <p class="stat-card-subheading mb-3">
+            Add Liquidity Tax Free Section
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default index;
